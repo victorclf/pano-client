a3fde708bc56c0ce1366f6bcfa10484f30a99682
Victor Freire <victorclf@live.com>
2022-08-01 16:11:05 -0300

    Update readme

--- src/mocks/mockData.js
+++ src/mocks/mockData.js
@@ -232,9 +232,16 @@ const createReadmePost = () => {
         title: "README: What's this app?",
         author,
         body: `
-            Pano is a demo of a social news app written with Typescript, React, Redux (+ RTK) and MUI.
+            Pano is a demo of a social news app written with TS, React, Redux (+ RTK Query) and MUI.
 
             Currently, the backend is a mock built with MSW, so the data is local and only lasts until the app is refreshed. Moreover, the response delays are artificial.
+
+            Features:
+            - Voting (up, down, unvote)
+            - Posts (infinite scroll listing, create and view)
+            - Comments (create, edit and view)
+            - Users (view)
+            - Auth (log in and out)
         `.trim(),
         date: new Date().toISOString(),
         score: 999
